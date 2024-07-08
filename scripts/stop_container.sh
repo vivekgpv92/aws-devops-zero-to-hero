aab1fdce07e0578619142012a5cd9e50e7d18cdb
Vivek Viswanathan <55182062+vivekgpv92@users.noreply.github.com>
2024-07-08 18:39:22 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,10 +2,10 @@
 set -e
 
 #Get the running container id
-container_id=$(docker ps | awk -F " " 'NR>1 {print $1}')
+container_id=$(docker ps -q)
 
 # Stop the running container (if any)
-if[ -n "$container_id" ];then
+if [ -n "$container_id" ]; then
   echo "stopping and removing containers..."
   docker stop $container_id
   docker rm -f $container_id

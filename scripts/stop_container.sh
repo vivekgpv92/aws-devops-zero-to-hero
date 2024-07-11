1e5443479eba7599127e3ab91f1b04b9eca61756
Vivek Viswanathan <55182062+vivekgpv92@users.noreply.github.com>
2024-07-11 17:16:15 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
 
-# Get the list of running container IDs
-container_id=$(docker ps -q)
-
 #Stop container
-docker stop $container_id
-docker rm $container_id
+docker rm -f $(docker ps -q)
